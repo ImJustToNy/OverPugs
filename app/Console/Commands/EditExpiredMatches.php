4e3b13a92aa777d4875b45e7466e7a872e357f68
ImJustToNy <imtony@protonmail.com>
2017-04-16 23:22:47 +0200

    Resolve discord only on production

--- app/Console/Commands/EditExpiredMatches.php
+++ app/Console/Commands/EditExpiredMatches.php
@@ -4,6 +4,7 @@ namespace OverPugs\Console\Commands;
 
 use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
 use OverPugs\Match;
 
 class EditExpiredMatches extends Command
@@ -40,14 +41,15 @@ class EditExpiredMatches extends Command
     public function handle()
     {
         $matches = Match::where('expireAt', '<', Carbon::now())->where('message_deleted', false)->get();
-        $discord = resolve('RestCord\DiscordClient');
 
         foreach ($matches as $match) {
-            $discord->channel->editMessage([
-                'channel.id' => intval(env('DISCORD_CHANNELID')),
-                'message.id' => intval($match->message_id),
-                'content' => ':no_entry_sign: Expired',
-            ]);
+            if (!App::environment('testing')) {
+                resolve('RestCord\DiscordClient')->channel->editMessage([
+                    'channel.id' => intval(env('DISCORD_CHANNELID')),
+                    'message.id' => intval($match->message_id),
+                    'content' => ':no_entry_sign: Expired',
+                ]);
+            }
 
             $match->message_deleted = true;
             $match->save();
