5cbda2913f510dc47f5e3478f57da6d3facef0cd
ImJustToNy <imtony@protonmail.com>
2017-05-14 21:26:55 +0200

    Removed ad tag from webpage

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,9 +7,7 @@
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-
+        
         <script>
             window.OverPugs = {!! json_encode([
                 'csrfToken' => csrf_token(),
@@ -30,8 +28,6 @@
         <script src="{{ mix('js/app.js') }}"></script>
 
         <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
